61518d67f2c58ca72b593b7b1273971fe8aaa9f6
JosseGonnza <accion.quevedo@gmail.com>
2025-12-10 18:48:30 +0000

    💫 feat: add Spring Boot application entry point

---
+++ src/main/java/org/jossegonnza/kitchenpantry/api/KitchenPantryApplication.java
@@ -0,0 +1,12 @@
+package org.jossegonnza.kitchenpantry.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "org.jossegonnza.kitchenpantry")
+public class KitchenPantryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenPantryApplication.class, args);
+    }
+}
