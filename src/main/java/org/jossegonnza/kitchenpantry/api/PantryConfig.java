849864c3fa93c4c0efbe0b2b392c18e994a1a732
JosseGonnza <accion.quevedo@gmail.com>
2025-12-14 21:12:50 +0000

    🏗️ config: inject JDBC repository into Pantry bean

--- src/main/java/org/jossegonnza/kitchenpantry/api/PantryConfig.java
+++ src/main/java/org/jossegonnza/kitchenpantry/api/PantryConfig.java
@@ -1,13 +1,14 @@
 package org.jossegonnza.kitchenpantry.api;
 
 import org.jossegonnza.kitchenpantry.domain.Pantry;
+import org.jossegonnza.kitchenpantry.infrastructure.persistence.jdbc.JdbcPantryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PantryConfig {
     @Bean
-    public Pantry pantry() {
-        return new Pantry();
+    public Pantry pantry(JdbcPantryRepository repository) {
+        return new Pantry(repository);
     }
 }
